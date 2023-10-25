250e9630896ea56b1401c57543d6834a8c78699a
RETA <msongereta@gmail.com>
2023-10-25 15:50:43 +0300

    Create rev.py

---
+++ rev.py
@@ -0,0 +1,10 @@
+name= input("enter your name")
+if "e" in name:
+    print(" the letter e exists")
+    print("the letter a does not exists")
+    name= "emanuel"
+    print (name[2:4])
+name= input("emmanuel")
+print(name.upper)
+myLivingRoom=["tv","table","chair"]
+print(myLivingRoom[0])
